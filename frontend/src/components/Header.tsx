353bfc1b760b1524030ecba2c5624798c1ad0a74
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-05-27 17:16:52 +0530

    MobileNav Done

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return <div className="border-b-2 border-b-orange-500 py-6">
@@ -6,6 +7,9 @@ const Header = () => {
         <Link to={""} className='text-3xl font-bold tracking-tight text-orange-500' >
             GoFood.com
         </Link>
+        <div className="md:hidden">
+          <MobileNav/>
+        </div>
     </div>
   </div>;
 };
