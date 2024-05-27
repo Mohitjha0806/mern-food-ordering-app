ee8421724a5b9973bade2da8ca08a078f5a9c10b
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-05-27 18:36:20 +0530

    responsive landing page Done

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,14 +1,22 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/layout";
+import Homepage from "./pages/Homepage";
 
-const AppRoutes = ()=>{
-    return(
-        <Routes>
-            <Route path="/" element={<Layout>Home Page</Layout>}/>
-            <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>}/>
-            <Route path="*" element={<Navigate to="/" />}/>
-        </Routes>
-    )
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <Homepage />
+          </Layout>
+        }
+      />
+      <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
 
 export default AppRoutes;
